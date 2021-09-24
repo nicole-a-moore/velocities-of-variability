93694797cb796f9acc09163aea469d824f9b5de0
nicole-a-moore <nicole.moore@mail.mcgill.ca>
2021-09-24 17:52:53 -0400

    updating job sub script for 18

---
+++ data-raw/sh_scripts/02_job_sub_script_18.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --account=def-jsunday
+#SBATCH --mem-per-cpu=2GB
+#SBATCH --time=24:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user='nicole.moore@mail.mcgill.ca'
+
+module load gcc/9.3.0 r/4.0.2 netcdf/4.7.4
+
+chosen_one=$1
+
+Rscript 02_resampling-GCMs_18.R $chosen_one
