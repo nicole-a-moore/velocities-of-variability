6ac3173e1cf3c68e7ced5a09992ccdede37f78d2
nicole-a-moore <nicole.moore@mail.mcgill.ca>
2021-11-12 10:37:17 -0500

    adding boxplots

--- 10_workflow_gcm-1.R
+++ 10_workflow_gcm-1.R
@@ -203,9 +203,15 @@ map_legend <- data.frame(rasterToPoints(legend_rast)) %>%
   scale_fill_manual(values = pal_realm) +
   guides(fill = "none")
 
-
-
-## conclusions:
+## boxplot:
+df %>%
+  ggplot(., aes(y = mean_spec_exp, x = realm, fill = realm)) + 
+  geom_boxplot() +
+  theme_light() +
+  labs(y = "Mean local spectral exponent", x = "") +
+  scale_fill_manual(values = pal_realm) +
+  guides(fill = "none") +
+  scale_x_discrete(labels = c("Land", "Ocean"))
 
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
@@ -264,7 +270,16 @@ map_legend <- data.frame(rasterToPoints(legend_rast)) %>%
   guides(fill = "none") +
   geom_polygon(data = countries, col="transparent", size = 0.01, fill = "transparent", alpha = 0.2,
                aes(x=long, y=lat, group = group)) 
-  
+
+## boxplot:
+df %>%
+  ggplot(., aes(y = mean_spec_exp, x = latitudinal_region, fill = latitudinal_region)) + 
+  geom_boxplot() +
+  theme_light() +
+  labs(y = "Mean local spectral exponent", x = "") +
+  scale_fill_manual(values = pal_lat) +
+  guides(fill = "none") +
+  scale_x_discrete(labels = c("Temperate", "Tropical"))
 
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
@@ -323,8 +338,14 @@ map_df %>%
   theme(legend.title = element_text(size = 8),
         legend.text = element_text(size = 6))
 
-
-
+## boxplot:
+df %>%
+  ggplot(., aes(y = slope_spec_exp, x = realm, fill = realm)) + 
+  geom_boxplot() +
+  theme_light() +
+  labs(y = "Mean local spectral exponent", x = "") +
+  scale_fill_manual(values = pal_realm) +
+  guides(fill = "none")
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
 ##### 2b. Analyze change in spectral exponent across tropical vs. temperate latitudes #####
@@ -388,5 +409,108 @@ map_df %>%
         legend.text = element_text(size = 6))  +
   geom_polygon(data = countries, col="black", size = 0.1, fill = "transparent", alpha = 0.1,
                aes(x=long, y=lat, group = group)) 
+
+## boxplot:
+df %>%
+  ggplot(., aes(y = slope_spec_exp, x = latitudinal_region, fill = latitudinal_region)) + 
+  geom_boxplot() +
+  theme_light() +
+  labs(y = "Mean local spectral exponent", x = "") +
+  scale_fill_manual(values = pal_lat) +
+  guides(fill = "none")
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+#####           2c. Tropical vs. temperate latitudes x land vs. ocean                 #####
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+## use mosaic_specchange
+mosaic_specchange <- readRDS("data-processed/01_tos-tas-mosaic_spectral-change.rds")
+
+## split data into tropics and temperate latitudes
+tropical <- data.frame(rasterToPoints(mosaic_specchange)) %>%
+  filter(y <= 23.5 & y >= -23.5) %>%
+  rasterFromXYZ(.)
+
+temperate <- data.frame(rasterToPoints(mosaic_specchange)) %>%
+  filter(y > 23.5 | y < -23.5) %>%
+  rasterFromXYZ(.)
+
+## split tropical and temperate into land and ocean
+tropical_land <- crop(land_mask, tropical)
+tropical_land <- mask(tropical, tropical_land)
+tropical_ocean <- mask(tropical, tropical_land, inverse = T)
+plot(tropical_land[[1]])
+plot(tropical_ocean[[1]])
+
+temperate_land <- crop(land_mask, temperate)
+temperate_land <- mask(temperate, temperate_land)
+temperate_ocean <- mask(temperate, temperate_land, inverse = T)
+plot(temperate_land[[1]])
+plot(temperate_ocean[[1]])
+
+## summary stats and figures:
+## mean
+mean(values(tropical_land[[2]]), na.rm = T)
+mean(values(temperate_land[[2]]), na.rm = T)
+mean(values(tropical_ocean[[2]]), na.rm = T)
+mean(values(temperate_ocean[[2]]), na.rm = T)
+
+## standard deviation
+sd(values(tropical_land[[2]]), na.rm = T)
+sd(values(temperate_land[[2]]), na.rm = T)
+sd(values(tropical_ocean[[2]]), na.rm = T)
+sd(values(temperate_ocean[[2]]), na.rm = T)
+
+## histogram - tropical vs. temperate 
+df1 <- data.frame(Land = c(values(tropical_land[[2]])), 
+                  Ocean = c(values(tropical_ocean[[2]])))
+df1 <- gather(df1, key = "realm", value = "slope_spec_exp", Land, Ocean)
+df1$trop_or_temp = "Tropical"
+df2 <- data.frame(Land = c(values(temperate_land[[2]])),
+                 Ocean = c(values(temperate_ocean[[2]])))
+df2 <- gather(df2, key = "realm", value = "slope_spec_exp", Land, Ocean)
+df2$trop_or_temp = "Temperate"
+df <- rbind(df1, df2) %>% 
+  filter(!is.na(slope_spec_exp))
+df$lat_and_realm = paste(df$trop_or_temp, df$realm, sep = "_")
+
+df %>%
+  ggplot(., aes(x = slope_spec_exp, fill = lat_and_realm)) + 
+  geom_histogram(position = position_dodge()) +
+  theme_light() +
+  labs(x = "Slope of spectral exponent",
+       y = "Frequency",
+       fill = "Latitude and realm") +
+  facet_wrap(~realm) +
+  scale_fill_discrete(labels = c("Temperate land", "Temperate ocean", "Tropical land", "Tropical ocean"))
+
+## map it!
+trop_land_df <- data.frame(rasterToPoints(tropical_land))
+trop_land_df$lat_and_realm = "Tropical_land"
+temp_land_df <- data.frame(rasterToPoints(temperate_land))
+temp_land_df$lat_and_realm = "Temperate_land"
+trop_ocean_df <- data.frame(rasterToPoints(tropical_ocean))
+trop_ocean_df$lat_and_realm = "Tropical_ocean"
+temp_ocean_df <- data.frame(rasterToPoints(temperate_ocean))
+temp_ocean_df$lat_and_realm = "Temperate_ocean"
+map_df <- rbind(temp_land_df,trop_ocean_df, trop_land_df, temp_ocean_df)
+
+map_df %>%
+  ggplot(., aes(x = x, y = y, fill = lat_and_realm)) + 
+  geom_raster() +
+  coord_fixed() +
+  theme_void() +
+  labs(fill = "Slope of\nspectral\nexponent") +
+  theme(legend.title = element_text(size = 8),
+        legend.text = element_text(size = 6))  
+
+## boxplot:
+df %>%
+  ggplot(., aes(y = slope_spec_exp, x = lat_and_realm, fill = lat_and_realm)) + 
+  geom_boxplot() +
+  theme_light() +
+  labs(y = "Mean local spectral exponent", x = "") +
+  guides(fill = "none") +
+  scale_x_discrete(labels = 
+                        c("Temperate land", "Temperate ocean", "Tropical land", "Tropical ocean"))
 
 
