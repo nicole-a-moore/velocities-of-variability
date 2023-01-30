07abf6e9026f3d34d5835f39d104775a8a97b08b
Nikki Moore <nicole.moore@mail.mcgill.ca>
2023-01-30 12:31:10 -0500

    wget script flow update

---
+++ data-raw/sh_scripts/wget_all_tos.sh
@@ -0,0 +1,24 @@
+# script to download all air GCM air temperature data using wget scripts
+
+#sh wget_01_CMCC-CMS_historical.sh
+#sh wget_01_CMCC-CMS_rcp85.sh
+sh wget_02_GFDL-CM3_historical.sh
+sh wget_02_GFDL-CM3_rcp85.sh
+sh wget_03_GFDL-ESM2G_historical.sh
+sh wget_03_GFDL-ESM2G_rcp85.sh
+# sh wget_04_HadGEM2-ES_historical.sh
+# sh wget_04_HadGEM2-ES_rcp85.sh
+# sh wget_05_INM-CM4_historical.sh
+# sh wget_05_INM-CM4_rcp85.sh
+# sh wget_06_IPSL-CM5A-MR_historical.sh
+# sh wget_06_IPSL-CM5A-MR_rcp85.sh
+# sh wget_07_MIROC-ESM-CHEM_historical.sh
+# sh wget_07_MIROC-ESM-CHEM_rcp85.sh
+# sh wget_08_MIROC5_historical.sh
+# sh wget_08_MIROC5_rcp85.sh
+# sh wget_09_MPI-ESM-LR_historical.sh
+# sh wget_09_MPI-ESM-LR_rcp85.sh
+# sh wget_10_MPI-ESM-MR_historical.sh
+# sh wget_10_MPI-ESM-MR_rcp85.sh
+# sh wget_11_MRI-CGCM3_historical.sh
+# sh wget_11_MRI-CGCM3_rcp85.sh
